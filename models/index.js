14da398e2b39994a9658a3ef875116480f087d27
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-15 17:15:40 -0500

    added associations to user,blog, and comments

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require ('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(COmment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+}); 
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports= {User, Post, Comment}; 
