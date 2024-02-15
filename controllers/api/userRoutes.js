7c44a6445cdef93f18d150d93c0a77aa1f1c5594
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-15 16:47:41 -0500

    sucessful login message added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -30,6 +30,13 @@ router.post('/login', async (req,res) => {
             return;
         }
 
-        
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: 'You are sucessfully logged in!'});
+        });
+    } catch (err) {
+        res.status(400).json(err);
     }
-})
+});
+
