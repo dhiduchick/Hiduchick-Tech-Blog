cced58906b3373e8077d220ef06145e5f2ebdaa4
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-19 11:00:11 -0500

    show add comment section

---
+++ public/js/comment.js
@@ -0,0 +1,11 @@
+const commentDiv = document.getElementById("comment-div");
+const commentLabel = document.getElementById("add-comment-label");
+const commentTextbox = document.getElementById("add-comment-textbox")
+
+
+function addCommentHandler() {
+  commentDiv.classList.remove("hide");
+  commentBtn.classList.add("hide");
+}
+
+
