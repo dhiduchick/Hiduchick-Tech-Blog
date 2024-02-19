187282a384cf87d3c1b1aab83c2f21eb155f4101
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-19 11:01:10 -0500

    call to api to post a new comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -9,3 +9,26 @@ function addCommentHandler() {
 }
 
 
+const submitCommentHandler = async (event) => {
+    event.preventDefault();
+    const content = commentTextbox.value.trim();
+    const post_id = document.querySelector('input').dataset.postid
+  
+    if (content) {
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ content, post_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to create project');
+      }
+    } else {
+      alert("Something is missing, please try again.")
+    }
+  }
